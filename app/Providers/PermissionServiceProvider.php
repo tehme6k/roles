2dff154221a0015eb092694c2ef510dd42c1e09a
tehme6k <moshimoshi6k@gmail.com>
2019-05-17 15:10:41 -0500

    blade directives to check role

--- app/Providers/PermissionServiceProvider.php
+++ app/Providers/PermissionServiceProvider.php
@@ -6,6 +6,7 @@ use Gate;
 use App\Permission;
 use App\Permissions;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class PermissionServiceProvider extends ServiceProvider
 {
@@ -30,6 +31,16 @@ class PermissionServiceProvider extends ServiceProvider
             Gate::define($permission->name, function ($user) use ($permission){
                 return $user->hasPermissionTo($permission);
             });
+        });
+
+        Blade::directive('role', function ($role){
+            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
+
+/*            return "<?php echo 'Hello ' . {$role}; ?>";*/
+        });
+
+        Blade::directive('endrole', function ($role){
+            return '<?php endif; ?>';
         });
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Show Topic #1</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
+                    @can('edit post')
+                        <a href="">Edit Post</a>
+                    @endcan
                 </div>
             </div>
         </div>
